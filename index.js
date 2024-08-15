b39aa22b7a8e9bb6f1195b73fc7185e6572eefe0
showrov4g <showrovg00@gmail.com>
2024-08-15 11:18:52 +0600

    all complete

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
-createBtn.addEventListener("click", ()=>{
+function showNotes() {
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+showNotes();
+
+function updateStorage() {
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+createBtn.addEventListener("click", () => {
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
     inputBox.className = "input-box";
@@ -11,8 +20,24 @@ createBtn.addEventListener("click", ()=>{
     notesContainer.appendChild(inputBox).appendChild(img);
 })
 
-notesContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "IMG"){
+notesContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "IMG") {
         e.target.parentElement.remove();
+        updateStorage();
+    }
+    else if (e.target.tagName === "p") {
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function () {
+                updateStorage();
+            }
+        })
+    }
+})
+
+document.addEventListener("keydown", event => {
+    if (event.key === "enter") {
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
     }
 })
